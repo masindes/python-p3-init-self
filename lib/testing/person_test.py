55ed3592c6dbedde42cbb971df1649b194f85e14
masinde sylvester <masinde.sylvester@yahoo.com>
2024-12-01 15:12:16 +0300

    initial commit

--- lib/testing/person_test.py
+++ lib/testing/person_test.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python3
-
-from person import Person
-
-class TestPerson:
-    '''Person in person.py'''
-
-    def test_is_class(self):
-        '''is a class with the name "Person"'''
-        guido = Person("Guido")
-        assert(type(guido) == Person)
-
-class TestInit:
-    '''Person.__init__ in person.py'''
-
-    def test_saves_self_name(self):
-        '''takes a name as an argument and saves it to self.name'''
-        guido = Person("Guido")
-        assert(guido.name == "Guido")
+class Person:
+    def __init__(self, name):
+        self.name = name
